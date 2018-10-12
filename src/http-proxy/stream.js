f74a7f9fd3db95a0a2cc6adc9dd2020eb6774936
quan <quan_xiaoxiao@qq.com>
2018-10-12 09:18:53 +0800

    xx

--- src/http-proxy/stream.js
+++ src/http-proxy/stream.js
@@ -1,4 +1,5 @@
 const { PassThrough } = require('stream');
+const _ = require('lodash');
 const http = require('http');
 
 module.exports = (ctx, options, emitError, setOutgoing = true) => {
@@ -7,46 +8,65 @@ module.exports = (ctx, options, emitError, setOutgoing = true) => {
   }
   console.log(`proxy: ${JSON.stringify(options)}`);
   const passThrough = new PassThrough();
-  const { req } = ctx;
-  const proxyReq = http.request(options);
+  const proxyReq = http.request(_.omit(options, ['body']));
 
   let proxyRes;
 
-  req.pipe(proxyReq)
-    .on('response', (res) => {
-      proxyRes = res;
+  proxyReq.on('response', (res) => {
+    proxyRes = res;
+    if (!ctx.res.finished) {
+      if (setOutgoing) {
+        ctx.status = proxyRes.statusCode;
+        ctx.set(proxyRes.headers);
+      }
+      proxyRes.pipe(passThrough);
+    }
+    proxyRes.on('error', () => {
       if (!ctx.res.finished) {
-        if (setOutgoing) {
-          ctx.status = proxyRes.statusCode;
-          ctx.set(proxyRes.headers);
-        }
-        proxyRes.pipe(passThrough);
-      }
-      proxyRes.on('error', () => {
-        if (!ctx.res.finished) {
-          passThrough.end();
-        }
-      });
-    })
-    .on('error', (error) => {
-      console.log(error);
-      if (proxyRes) {
-        proxyRes.unpipe(passThrough);
-      }
-      if (emitError) {
-        passThrough.emit('error', error);
-      } else if (!ctx.res.finished) {
-        ctx.status = 500;
         passThrough.end();
       }
     });
-
-  req.on('error', () => {
-    proxyReq.abort();
   });
 
-  req.on('aborted', () => {
-    proxyReq.abort();
+  proxyReq.on('error', (error) => {
+    console.log(error);
+    if (proxyRes) {
+      proxyRes.unpipe(passThrough);
+    }
+    if (emitError) {
+      passThrough.emit('error', error);
+    } else if (!ctx.res.finished) {
+      ctx.status = 500;
+      passThrough.end();
+    }
   });
+
+  if (options.body) {
+    if (options.body.pipe) {
+      const req = options.body;
+      req.pipe(proxyReq);
+      req.on('error', () => {
+        proxyReq.abort();
+      });
+
+      req.on('aborted', () => {
+        proxyReq.abort();
+      });
+    } else {
+      proxyReq.write(options.body);
+      proxyReq.end();
+    }
+  } else {
+    const { req } = ctx;
+    req.pipe(proxyReq);
+    req.on('error', () => {
+      proxyReq.abort();
+    });
+
+    req.on('aborted', () => {
+      proxyReq.abort();
+    });
+  }
+
   return passThrough;
 };
