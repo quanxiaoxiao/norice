919f058f36a448107284454440438e4f92fe3770
quan <quan_xiaoxiao@qq.com>
2019-10-22 11:54:41 +0800

    xxx

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -2,7 +2,7 @@ const logger = require('koa-logger');
 
 function ignoreAssects(mw) {
   return async function filter(ctx, next) {
-    if (/\.(js|png|jpg|css)$/.test(ctx.path) || /^\/OneMapServer/.test(ctx.path)) {
+    if (/\.(js|png|jpg|css)$/.test(ctx.path)) {
       await next();
     } else {
       await mw.call(this, ctx, next);
