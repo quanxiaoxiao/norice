1d4d940fb171845fdef8b019044f106b164fb8d2
quan <quan_xiaoxiao@qq.com>
2019-10-28 10:02:56 +0800

    xxx

--- src/actions/resources.js
+++ src/actions/resources.js
@@ -1,4 +1,6 @@
+const fp = require('lodash/fp');
 const moment = require('moment');
+const { table } = require('table');
 const compileModle = require('../lib/compileModle');
 const request = require('../lib/request');
 const getResourceRequestOptions = require('../lib/getResourceRequestOptions');
@@ -8,18 +10,36 @@ module.exports = async (configName) => {
   const mod = compileModle(configName);
   const { exports: config } = mod;
   try {
+    const currentBuf = await request({
+      ...getResourceRequestOptions(config),
+      path: '/resource',
+      method: 'GET',
+    });
+    const { _id } = JSON.parse(currentBuf);
     const buf = await request({
       ...getResourceRequestOptions(config),
       path: '/resources',
       method: 'GET',
     });
-    const data = JSON.parse(buf);
-    const list = data.map((item) => ({
-      ...item,
-      timeCreate: moment(item.timeCreate).format('YYYY-MM-DD HH:mm'),
-      list: item.list,
-    }));
-    console.log(JSON.stringify(list, null, 2));
+    fp.compose(
+      console.log,
+      (_) => table(
+        [
+          ['id', 'message', 'tag', 'file count', 'timeCreate'],
+          ..._,
+        ],
+        {},
+      ),
+      fp.map((item) => [
+        _id === item._id ? `* ${item._id}` : `  ${item._id}`,
+        item.message,
+        item.tag,
+        item.list.length,
+        moment(item.timeCreate).format('YYYY-MM-DD HH:mm'),
+      ]),
+      fp.orderBy(['timeCreate'], ['desc']),
+      JSON.parse,
+    )(buf);
   } catch (error) {
     console.log('fail');
   }
