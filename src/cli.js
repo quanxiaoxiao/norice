2005ac49c69f708e298de4e6ffb0c1afc44d7f4b
huzhedong <huzd@sunlandgroup.cn>
2022-02-09 13:49:08 +0800

    xxx

--- src/cli.js
+++ src/cli.js
@@ -18,52 +18,7 @@ yargs // eslint-disable-line
       default: 3000,
     }),
     (argv) => {
-      require('./actions/server')(argv.config, argv.port);
-    },
-  )
-  .command(
-    'deploy',
-    'deploy static file to server',
-    {
-      message: {
-        alias: 'm',
-        default: '',
-        type: 'string',
-      },
-      tag: {
-        alias: 't',
-        type: 'string',
-      },
-      dir: {
-        alias: 'd',
-        type: 'string',
-      },
-    },
-    (argv) => {
-      if (argv.dir) {
-        require('./actions/deployByDir')(argv.config, argv.dir, argv.message, argv.tag);
-      } else {
-        require('./actions/deploy')(argv.config, argv.message, argv.tag);
-      }
-    },
-  )
-  .command(
-    'resource',
-    'resource operation [action](show|select|fetch)',
-    {},
-    (argv) => {
-      const [, action, name] = argv._;
-      if (action === 'show') {
-        if (name) {
-          require('./actions/resources/printResource')(argv.config, name);
-        } else {
-          require('./actions/resources/printResources')(argv.config);
-        }
-      } else if (action === 'select' && name) {
-        require('./actions/resources/selectResource')(argv.config, name);
-      } else if (action === 'fetch') {
-        require('./actions/resources/fetchResource')(argv.config, name);
-      }
+      require('./server')(argv.config, argv.port);
     },
   )
   .version(pkg.version)
